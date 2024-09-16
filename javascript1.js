23da16e921c7625f2772a8a4420253bd5780ce49
selhz9 <sarahhantz9@gmail.com>
2024-09-16 18:41:38 -0500

    Update and rename script.js to javascript1.js

---
+++ javascript1.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+    const teamName = localStorage.getItem("teamName");
+    const avatar = localStorage.getItem("avatar");
+
+ 
+    if (teamName) {
+        document.getElementById("teamNameDisplay").textContent = `${teamName}'s Card`;
+    } else {
+        document.getElementById("teamNameDisplay").textContent = "Team Name: Not Found";
+    }
+
+    const teamAvatar = document.getElementById("teamAvatar");
+    if (avatar) {
+        teamAvatar.src = avatar;
+        teamAvatar.style.display = "block"; 
+    } else {
+        teamAvatar.style.display = "none"; 
+    }
+});
