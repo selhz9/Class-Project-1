a8549353380f0c7af15e4ce4b776fb3a36b8cfa3
ImThisHannah <hannahmyers510@gmail.com>
2024-09-16 19:25:33 -0400

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,25 @@ const avatarUrls = [
   constructor(cards = freshDeck()) {
     this.cards = cards;
   }
+const themeSwitcher = document.querySelector('#theme-switcher');
+const container = document.querySelector('.container');
 
+// Set default mode to dark
+let mode = 'dark';
+
+// Listen for a click event on toggle switch
+themeSwitcher.addEventListener('click', function () {
+  // If mode is dark, apply light background
+  if (mode === 'dark') {
+    mode = 'light';
+    container.setAttribute('class', 'light');
+  }
+  // If mode is light, apply dark background
+  else {
+    mode = 'dark';
+    container.setAttribute('class', 'dark');
+  }
+});
   get numberOfCards() {
     return this.cards.length;
   }
