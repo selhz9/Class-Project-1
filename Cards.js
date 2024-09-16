f8c3f374349ba02f0c33a72adb6260068e71e206
ImThisHannah <hannahmyers510@gmail.com>
2024-09-16 19:29:57 -0400

    Update Cards.js

--- Cards.js
+++ Cards.js
@@ -1,73 +1,20 @@
+document.addEventListener("DOMContentLoaded", function() {
 
-export default class Deck {
-  constructor(cards = freshDeck()) {
-    this.cards = cards;
-  }
+    const teamName = localStorage.getItem("teamName");
+    const avatar = localStorage.getItem("avatar");
 
-  get numberOfCards() {
-    return this.cards.length;
-  }
-
-  pop() {
-    if (this.numberOfCards === 0) {
-      throw new Error("No cards left in the deck.");
+ 
+    if (teamName) {
+        document.getElementById("teamNameDisplay").textContent = ${teamName}'s Card;
+    } else {
+        document.getElementById("teamNameDisplay").textContent = "Team Name: Not Found";
     }
-    return this.cards.pop(); // Changed to pop to remove the last card
-  }
 
-  push(card) {
-    this.cards.push(card);
-  }
-
-  shuffle() {
-    for (let i = this.numberOfCards - 1; i > 0; i--) {
-      const newIndex = Math.floor(Math.random() * (i + 1));
-      const oldValue = this.cards[newIndex];
-      this.cards[newIndex] = this.cards[i];
-      this.cards[i] = oldValue;
+    const teamAvatar = document.getElementById("teamAvatar");
+    if (avatar) {
+        teamAvatar.src = avatar;
+        teamAvatar.style.display = "block"; 
+    } else {
+        teamAvatar.style.display = "none"; 
     }
-  }
-}
-const SUITS = ["♠", "♣", "♥", "♦"]
-
-const VALUES = [
-  "A",
-  "2",
-  "3",
-  "4",
-  "5",
-  "6",
-  "7",
-  "8",
-  "9",
-  "10",
-  "J",
-  "Q",
-  "K"
-]
-class Card {
-  constructor(suit, value) {
-    this.suit = suit
-    this.value = value
-  }
-
-  get color() {
-    return this.suit === "♣" || this.suit === "♠" ? "black" : "red"
-  }
-
-  getHTML() {
-    const cardDiv = document.createElement("div")
-    cardDiv.innerText = this.suit
-    cardDiv.classList.add("card", this.color)
-    cardDiv.dataset.value = `${this.value} ${this.suit}`
-    return cardDiv
-  }
-}
-
-function freshDeck() {
-  return SUITS.flatMap(suit => {
-    return VALUES.map(value => {
-      return new Card(suit, value)
-    })
-  })
-}
+});
