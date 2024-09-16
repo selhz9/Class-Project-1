19ce587281f2ad6c73b35e4113e6dc30d00c1010
ImThisHannah <hannahmyers510@gmail.com>
2024-09-16 19:36:50 -0400

    Update script.js

--- script.js
+++ script.js
@@ -100,16 +100,14 @@ const text = document.querySelector(".text")
     const selectedAvatar = document.querySelector('input[name="avatar"]:checked');
   
     if (selectedAvatar) {
-      // Store the image URL in local storage
+      
       localStorage.setItem('avatar', selectedAvatar.value);
   
-      // Update the displayed avatar image
       const avatarPreview = document.getElementById('avatar-preview');
       avatarPreview.src = selectedAvatar.value;
     }
   }
   
-  // Function to display the stored avatar on page load
   function displayAvatar() {
     const storedAvatar = localStorage.getItem('avatar');
     if (storedAvatar) {
@@ -118,10 +116,8 @@ const text = document.querySelector(".text")
     }
   }
   
-  // Call the displayAvatar function on page load
 displayAvatar();
 
-// Display player and computer cards
 var img = document.createElement('img');
 img.src = "img/cards/" + playerHand[0] + ".png";
 
@@ -131,10 +127,8 @@ img2.src = "img/cards/" + compHand[0] + ".png";
 $('.playerCard').append(img).animateCss("flipInYRev");
 $('.compCard').append(img2).animateCss("flipInY");
 
-// Call the compare function to compare current cards
 compare(playerHand[0], compHand[0]);
 
-// Function to compare player and computer cards
 function compare(player, comp) {
     if ((player % 13) > (comp % 13)) {
         $('.result').html("Player wins!").animateCss("flipInX");
@@ -147,7 +141,6 @@ function compare(player, comp) {
     }
 }
 
-// Function to handle "war" instances or ties
 function war() {
     $('#warAnimation').addClass('war').show();
     $('#warAnimation').css("display", "table");
@@ -163,7 +156,6 @@ function war() {
     }, 2000);
 }
 
-// Function to move cards to a winner's deck with animation
 function moveCards(winner) {
     if (winner == "player") {
         $(".playerCard img").animate({ left: '-2000px' }, function() { $(this).hide(); });
@@ -178,7 +170,6 @@ function moveCards(winner) {
     }
 }
 
-// Function to handle war scenario and move cards to warArray
 function warToArray() {
     var length = Math.min(playerHand.length, compHand.length, 4);
 
@@ -187,13 +178,13 @@ function warToArray() {
         warArray.push(compHand.shift());
     }
 
-    // Additional logic for displaying cards and comparing
+
 }
 
 
 
 function compareWar(player, comp) {
-    // Reset the war array to empty
+
     warArray.length = 0;
 
     setTimeout(function() {
@@ -203,23 +194,19 @@ function compareWar(player, comp) {
 
     setTimeout(function() {
         $("#warArea").hide();
-        // Update card count and check for a winner
         updateCount();
         checkWin();
     }, 3500);
 }
 function compareWar(player, comp) {
     if ((player % 13) < (comp % 13)) {
-        // Update result section of the game board
         $('#result').html("Computer wins!");
         $('.result').html("Computer wins!");
 
-        // Push the entire war array to the back of the computer's hand
         compHand.push.apply(compHand, warArray);
     }
 }
 function compareWar(player, comp) {
-		//resets the war array to empty
 		warArray.length = 0;
 
 		setTimeout(function() {
@@ -231,18 +218,15 @@ function compareWar(player, comp) {
 			$("#warArea").hide();
 		}, 3500);
 
-		//update card count and check for a winner
 		updateCount();
 		checkWin();
     }
 function checkWin() {
 
-	//if player is out of cards, computer wins
 	if (playerHand.length == 0) {
 		alert("Computer wins. :(");
 		$(".result").html("The computer wins the game :(").animateCss("flipInX");
 
-		//resets the card and deck image to make it seem like the player is out of cards
 		$('.playerCard').html("");
     }
 }
@@ -251,29 +235,22 @@ function checkWin() {
         alert("Player wins!! :D");
         $(".result").html("You won the game! :)").animateCss("flipInX");
 
-        // Reset the computer's hand, card, and deck images
         $('.compHand').html("");
         $('.compCard').html("");
         $('.compDeck').html("");
     }
 }
-// Function to check win condition and update game state
 function checkWin() {
     if (compHand.length == 0) {
         alert("Player wins!! :D");
         $(".result").html("You won the game! :)").animateCss("flipInX");
-
-        // Reset the computer's hand, card, and deck images
         $('.compHand').html("");
         $('.compCard').html("");
         $('.compDeck').html("");
-
-        // Hide the "deal" button to force the player to start a new game
         $('#dealButton').hide();
     }
 }
 
-// Function to transition from "how to play" screen to game board
 function play() {
     hideAll();
     $('#desktop').show();
@@ -282,7 +259,6 @@ function play() {
     playing = true;
 }
 
-//function to update the card count after every "deal" finishes
 function updateCount() {
 	$('.playCount').html("Player cards: " + playerHand.length);
 	$('.compCount').html("Computer cards: " + compHand.length);
