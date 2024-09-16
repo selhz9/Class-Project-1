51c861139ab5541bffeebc0ed6f6e17db8f07158
ImThisHannah <hannahmyers510@gmail.com>
2024-09-16 17:55:04 -0400

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,79 @@ const avatarUrls = [
     "path/to/avatar3.svg",
     // ... more avatar URLs
   ];
-  
+  export default class Deck {
+  constructor(cards = freshDeck()) {
+    this.cards = cards;
+  }
+
+  get numberOfCards() {
+    return this.cards.length;
+  }
+
+  pop() {
+    if (this.numberOfCards === 0) {
+      throw new Error("No cards left in the deck.");
+    }
+    return this.cards.pop(); // Changed to pop to remove the last card
+  }
+
+  push(card) {
+    this.cards.push(card);
+  }
+
+  shuffle() {
+    for (let i = this.numberOfCards - 1; i > 0; i--) {
+      const newIndex = Math.floor(Math.random() * (i + 1));
+      const oldValue = this.cards[newIndex];
+      this.cards[newIndex] = this.cards[i];
+      this.cards[i] = oldValue;
+    }
+  }
+}
+const SUITS = ["♠", "♣", "♥", "♦"]
+
+const VALUES = [
+  "A",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K"
+]
+class Card {
+  constructor(suit, value) {
+    this.suit = suit
+    this.value = value
+  }
+
+  get color() {
+    return this.suit === "♣" || this.suit === "♠" ? "black" : "red"
+  }
+
+  getHTML() {
+    const cardDiv = document.createElement("div")
+    cardDiv.innerText = this.suit
+    cardDiv.classList.add("card", this.color)
+    cardDiv.dataset.value = `${this.value} ${this.suit}`
+    return cardDiv
+  }
+}
+
+function freshDeck() {
+  return SUITS.flatMap(suit => {
+    return VALUES.map(value => {
+      return new Card(suit, value)
+    })
+  })
+}
+
   // Get a reference to the image element
   const avatarImage = document.getElementById("avatar-image");                                                                                             function chooseAvatar() {
     // Get the selected avatar image element
