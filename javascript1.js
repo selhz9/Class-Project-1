96fe56c9bb38975b26553d05a2a34e1db5baa687
ImThisHannah <hannahmyers510@gmail.com>
2024-09-16 20:25:42 -0400

    Update javascript1.js

--- javascript1.js
+++ javascript1.js
@@ -18,3 +18,23 @@ document.addEventListener("DOMContentLoaded", function() {
         teamAvatar.style.display = "none"; 
     }
 });
+
+const themeSwitcher = document.querySelector('#theme-switcher');
+const container = document.querySelector('.container');
+
+
+let mode = 'dark';
+
+
+themeSwitcher.addEventListener('click', function () {
+  
+  if (mode === 'dark') {
+    mode = 'light';
+    container.setAttribute('class', 'light');
+  }
+  
+  else {
+    mode = 'dark';
+    container.setAttribute('class', 'dark');
+  }
+});
