14bec42fdd6fe6ebb2dd0a2309315566c14de57c
selhz9 <sarahhantz9@gmail.com>
2024-09-16 20:43:32 -0500

    Update page1javascript.js

--- page1javascript.js
+++ page1javascript.js
@@ -1,29 +1,26 @@
-
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", () => {
     const form = document.getElementById("teamForm");
     const avatarInput = document.getElementById("avatar");
 
-    form.addEventListener("submit", function(event) {
+    form.addEventListener("submit", (event) => {
         event.preventDefault(); 
 
-        
         const teamName = document.getElementById("teamName").value;
-
-        
         const file = avatarInput.files[0];
-        let fileReader = new FileReader();
-        
-        fileReader.onloadend = function () {
-            
-            localStorage.setItem("teamName", teamName);
-            localStorage.setItem("avatar", fileReader.result);
-            
-            window.location.href = "page2.html";
-        };
-        
+
         if (file) {
-            fileReader.readAsDataURL(file);
+            const reader = new FileReader();
+
+            reader.onloadend = () => {
+
+                localStorage.setItem("teamName", teamName);
+                localStorage.setItem("avatar", reader.result);
+             
+                window.location.href = "page2.html";
+            };
+            reader.readAsDataURL(file);
         } else {
+    
             localStorage.setItem("teamName", teamName);
             localStorage.setItem("avatar", "");
             window.location.href = "page2.html";
